c308d51d9ef2bd480a2c969c5f1371fb865d3497
Selvamurugan Senthilkumar <97260536+Selva0309@users.noreply.github.com>
2024-11-24 01:46:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	
+	const firstName = document.getElementsByName('fname')[0].value;
+    const lastName = document.getElementsByName('lname')[0].value;
+	console.log(firstName,lastName);
+	alert(`${firstName}${lastName}`);
 
 }
